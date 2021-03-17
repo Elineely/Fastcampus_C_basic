7bd4be345db93648d70bb9a0f30d089134cf8137
Elineely <surinlee@Surinui-MacBook-Air.local>
2021-03-17 16:13:12 +0900

    Create ch3_2.c

---
+++ ch3_2.c
@@ -0,0 +1,11 @@
+#두 숫자(정수)를 입력받아 순서 바꾸어 출력하기
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d %d\n", b, a);
+    system("pause");
+    return 0;
+}
